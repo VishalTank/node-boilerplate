c0233465966d90142dd7091e9ad907e6ec9720b2
Vishal Tank <vishal.97t@gmail.com>
2021-04-13 01:06:06 +0530

    Added: User Schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,49 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = mongoose.Schema({});
+const { roles } = require('../config/roles');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email');
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+        validate(value) {
+            if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+                throw new Error('Password must contain at least one letter and one number');
+            }
+        },
+    },
+    role: {
+        type: String,
+        enum: roles,
+        default: 'user',
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
 
 const User = mongoose.model('User', userSchema);
 
