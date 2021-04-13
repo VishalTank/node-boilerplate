f9162b0351a5a6ccaaaff108d7654f8585ce75d8
Vishal Tank <vishal.97t@gmail.com>
2021-04-13 18:45:33 +0530

    Added: Request body validation utilities

---
+++ src/utils/pick.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line arrow-body-style
+const pick = (object, keys) => {
+    return keys.reduce((obj, key) => {
+
+        if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+            // eslint-disable-next-line no-param-reassign
+            obj[key] = object[key];
+        }
+
+        return obj;
+    }, {});
+};
+
+module.exports = pick;
